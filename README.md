a5b6dd78049a2aff61c6e6e773eafcce38a54046
HtLord <s75757775@hotmail.com>
2019-05-21 21:30:14 +0800

    Modify models and add some library 1. Add filter struct for member model 1. Add some library 1. Modify README

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 Ontnua
 ---
+#### What Is
+
+---
+#### Whys
+
+`gin-gonic`
+- For helping mapping form to struct
+- 
+
+
+---
+### Tips
+- Using `$./acp.sh` to add all, commit all, and push to github.
+- Using `$./acpp.sh` to  add all, commit all, push to github,
+ and push to heroku.
+ 
+---
 #### CAUSTION
-Used env
+##### Env vars
 - GIN_MODE
 - MONGO_SECRET
 - PORT
--- model/roles.go
+++ model/roles.go
@@ -10,10 +10,22 @@ const (
 	ROOT_ADM ADMLevel = 0
 )
 
+// We can use required feature to validate `must be fulfilled field`
 type Member struct {
-	Name string `json:"name" form:"name" xml:"name" binding:"required"`
-	Email string `json:"email" form:"email" xml:"email" binding:"required"`
-	Secret string `json:"secret" form:"secret" xml:"secret" binding:"required"`
+	Name       string    `json:"name" form:"name" xml:"name" binding:"required"`
+	Email      string    `json:"email" form:"email" xml:"email" binding:"required"`
+	Secret     string    `json:"secret" form:"secret" xml:"secret" binding:"required"`
+	Enable     bool      `json:"enable" form:"enable" xml:"enable"`
+	CreateTime time.Time `json:"createTime" form:"createTime" xml:"createTime"`
+	UpdateTime time.Time `json:"updateTime" form:"updateTime" xml:"updateTime"`
+}
+
+// Filter match secret will has security problem. Some may trend to using readable secret
+// to find same results Name, Email.
+type MemberFilter struct {
+	Name       string    `json:"name" form:"name" xml:"name"`
+	Email      string    `json:"email" form:"email" xml:"email"`
+	Enable     bool      `json:"enable" form:"enable" xml:"enable"`
 	CreateTime time.Time `json:"createTime" form:"createTime" xml:"createTime"`
 	UpdateTime time.Time `json:"updateTime" form:"updateTime" xml:"updateTime"`
 }
@@ -24,8 +36,8 @@ type ADM struct {
 }
 
 type Store struct {
-	BAN string `msgpack:"ban" json:"ban" form:"ban" xml:"ban" binding:"required"`
-	Name string `msgpack:"name" json:"name" form:"name" xml:"name" binding:"required"`
-	ADMs []ADM `msgpack:"adms" json:"adms" form:"adms" xml:"adms" binding:"required"`
+	BAN     string   `msgpack:"ban" json:"ban" form:"ban" xml:"ban" binding:"required"`
+	Name    string   `msgpack:"name" json:"name" form:"name" xml:"name" binding:"required"`
+	ADMs    []ADM    `msgpack:"adms" json:"adms" form:"adms" xml:"adms" binding:"required"`
 	Members []Member `msgpack:"members" json:"members" form:"members" xml:"members" binding:"required"`
 }
